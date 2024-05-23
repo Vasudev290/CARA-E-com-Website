8b72f8b19fcfa62bde4783f8a932a130e00aa922
vasudev290 <vasukesavulu@gmail.com>
2024-05-23 19:34:21 +0530

    Frontend interface

---
+++ script.js
@@ -0,0 +1,27 @@
+const bar = document.getElementById('bar');
+const close = document.getElementById('close');
+const nav = document.getElementById('navbar');
+
+if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    close.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+}
+
+/* if (bar) {
+    bar.addEventListener('click', () => {
+        nav.classList.add('active');
+    })
+}
+
+if (close) {
+    bar.addEventListener('click', () => {
+        nav.classList.remove('active');
+    })
+} */
